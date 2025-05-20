340e4cc1fb85686d0bc5a251da68100590a2ca29
Pawan Senpura <86240715+pawan-live@users.noreply.github.com>
2025-05-20 15:04:07 +0530

    Update AgendaData.ts

--- src/data/AgendaData.ts
+++ src/data/AgendaData.ts
@@ -1,89 +1,88 @@
 export const AgendaData = [
   {
     title: "Welcome Speech",
-    time: "11:00 AM",
+    time: "10:00 AM",
     imageURL: "/faces/msthilini.jpeg",
     name: "Ms. Thilini Jayalath",
     position: "Senior Lecturer",
   },
   {
+    title: "Showreel Video 2024/25",
+    time: "10:05 AM",
+  },
+  {
     title: "Year Resolution by Past President",
-    time: "11:10 AM",
+    time: "10:10 AM",
     imageURL: "/faces/dinal.jpg",
     name: "Pasindu Dinal",
     position: "President, SESC 2024/25",
   },
   {
-    title: "Showreel Video 2024/25",
-    time: "11:20 AM",
+    title: "Speech by Prof. Nuwan Kodagoda",
+    time: "10:20 AM",
+    imageURL: "/faces/nuwan.jpg",
+    name: "Prof. Nuwan Kodagoda",
+    position: "Pro Vice Chancellor, SLIIT",
+  },
+  {
+    title: "Speech by Prof. Pradeep Abeygunawardhana",
+    time: "10:30 AM",
+    imageURL: "/faces/pradeep.jpg", // Pradeep sir's image
+    name: "Prof. Pradeep Abeygunawardhana",
+    position: "Dean, Faculty Of Computing, SLIIT",
   },
   {
     title: "Year Plan for 2025/26",
-    time: "11:25 AM",
+    time: "10:40 AM",
     imageURL: "/faces/pawan.png", // Pawan's image
     name: "Pawan Senpura",
     position: "New President, SESC 2025/26",
   },
   {
     title: "Speech by Dr. Kalpani Manathunga",
-    time: "11:40 AM",
+    time: "10:50 AM",
     imageURL: "/faces/kalpani.jpg",
     name: "Dr. Kalpani Manathunga",
     position: "HOD, SE Department of SLIIT",
   },
   {
-    title: "Speech by Prof. Nuwan Kodagoda",
-    time: "11:55 AM",
-    imageURL: "/faces/nuwan.jpg",
-    name: "Prof. Nuwan Kodagoda",
-    position: "Pro Vice Chancellor, SLIIT",
-  },
-  {
-    title: "Awarding Appreciation Letters for Past Volunteers",
-    time: "12:10 PM",
+    title: "Volunteer Appreciation 2024/25",
+    time: "11:00 AM",
   },
   {
     title: "Speech by Sandithi Nethsiluni- Volunteer Experience",
-    time: "12:25 PM",
+    time: "11:10 AM",
     imageURL: "/faces/sandithi.jpeg", // Sandithi's image
     name: "Sandithi Nethsiluni",
     position: "Committee Member, SESC 2025/26",
   },
   {
     title: "Speech by Asiri Jayawardena",
-    time: "12:35 PM",
+    time: "11:20 AM",
     imageURL: "/faces/asiri.jpeg", // Asiri's image
     name: "Asiri Jayawardena",
     position: "Vice President, SESC 2025/26",
   },
   {
-    title: "Speech by Prof. Pradeep Abeygunawardhana",
-    time: "12:50 PM",
-    imageURL: "/faces/pradeep.jpg", // Pradeep sir's image
-    name: "Prof. Pradeep Abeygunawardhana",
-    position: "Dean, Faculty Of Computing, SLIIT",
-  },
-  {
     title: "Awarding Service Letters for Past Members",
-    time: "1:05 PM",
+    time: "11:30 AM",
   },
   {
     title: "Speech by President of FCSC",
-    time: "1:20 PM",
+    time: "11:45 AM",
     imageURL: "/faces/chathurma.jpeg", // FCSC President's image
     name: "Chathurma Jayawickrama",
     position: "President, FCSC 2025/26",
   },
   {
     title: "Awarding Appointment Letters for New Members",
-    time: "1:35 PM",
+    time: "11:55 AM",
   },
   {
     title: "Vote of Thanks",
-    time: "1:50 PM",
+    time: "12:10 PM",
     imageURL: "/faces/sathnindu.jpeg", // Sathnindu's image
     name: "Sathnindu Kottage",
     position: "Secretary, SESC 2025/26",
   },
 ];
-
